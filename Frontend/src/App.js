4ebfbf03c6de145145325a5462b1433dca98517d
Rishu Ranjan <121298780+RishuRanjan11@users.noreply.github.com>
2025-07-05 11:26:55 +0530

    Add files via upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,79 +1,110 @@
-import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Carousel from "./components/Carousel";
-import Products from "./components/Products";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import AboutPage from "./pages/AboutPage";
-import ServicesPage from "./pages/ServicesPage";
-import ProductsPage from "./pages/ProductsPage";
-import ContactPage from "./pages/ContactPage";
-import LoginPage from "./pages/LoginPage";
-import "./App.css";
-import SignupPage from "./pages/SignupPage";
-import { useUserStore } from "./stores/useUserStore";
-import { Toaster } from "react-hot-toast";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+import Products from './components/Products';
+import About from './components/About';
+import Footer from './components/Footer';
+import AboutPage from './pages/AboutPage';
+import ServicesPage from './pages/ServicesPage';
+import ProductsPage from './pages/ProductsPage';
+import ContactPage from './pages/ContactPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import { CartProvider } from './context/CartContext';
+import AdminLogin from './pages/Admin/AdminLogin';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import AdminProducts from './pages/Admin/AdminProducts';
+import AdminOrders from './pages/Admin/AdminOrders';
+import AdminCoupons from './pages/Admin/AdminCoupons';
+import AdminUsers from './pages/Admin/AdminUsers';
+import ProtectedAdminRoute from './components/ProtectedAdminRoute';
+import './App.css';
 
 function App() {
-  const checkAuth = useUserStore((state) => state.checkAuth);
-
-  useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
-
   return (
     <>
-      <Toaster position="top-center" reverseOrder={false} />
       {/* Social Links */}
       <div className="social-links top-links">
-        <a href="https://www.instagram.com/">
-          <img
-            src="https://img.icons8.com/color/30/instagram.png"
-            alt="Instagram"
-          />
-        </a>
-        <a href="#">
-          <img
-            src="https://img.icons8.com/color/30/facebook-new.png"
-            alt="Facebook"
-          />
-        </a>
+        <a href="https://www.instagram.com/"><img src="https://img.icons8.com/color/30/instagram.png" alt="Instagram" /></a>
+        <a href="#"><img src="https://img.icons8.com/color/30/facebook-new.png" alt="Facebook" /></a>
       </div>
       <div className="social-links bottom-links">
-        <a href="https://wa.me/+916203108650">
-          <img
-            src="https://img.icons8.com/color/30/whatsapp.png"
-            alt="WhatsApp"
-          />
-        </a>
-        <a href="tel:+911234567890">
-          <img src="https://img.icons8.com/color/30/phone.png" alt="Call" />
-        </a>
+        <a href="https://wa.me/+916203108650"><img src="https://img.icons8.com/color/30/whatsapp.png" alt="WhatsApp" /></a>
+        <a href="tel:+911234567890"><img src="https://img.icons8.com/color/30/phone.png" alt="Call" /></a>
       </div>
 
-      <Navbar />
-      <Routes>
-        {/* Home Route */}
-        <Route
-          path="/"
-          element={
-            <>
-              <Carousel />
-              <Products />
-              <About />
-              <Footer />
-            </>
-          }
-        />
-        {/* About Route */}
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
+      <CartProvider>
+        <Navbar />
+
+        <Routes>
+          {/* Customer Routes */}
+          <Route
+            path="/"
+            element={
+              <>
+                <Carousel />
+                <Products />
+                <About />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/services" element={<ServicesPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+
+          {/* Admin Routes */}
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route
+            path="/admin/dashboard"
+            element={
+              <ProtectedAdminRoute>
+                <AdminDashboard />
+              </ProtectedAdminRoute>
+            }
+          />
+          <Route
+            path="/admin/products"
+            element={
+              <ProtectedAdminRoute>
+                <AdminProducts />
+              </ProtectedAdminRoute>
+            }
+          />
+          <Route
+            path="/admin/orders"
+            element={
+              <ProtectedAdminRoute>
+                <AdminOrders />
+              </ProtectedAdminRoute>
+            }
+          />
+          <Route
+            path="/admin/coupons"
+            element={
+              <ProtectedAdminRoute>
+                <AdminCoupons />
+              </ProtectedAdminRoute>
+            }
+          />
+          <Route
+            path="/admin/users"
+            element={
+              <ProtectedAdminRoute>
+                <AdminUsers />
+              </ProtectedAdminRoute>
+            }
+          />
+        </Routes>
+      </CartProvider>
     </>
   );
 }
