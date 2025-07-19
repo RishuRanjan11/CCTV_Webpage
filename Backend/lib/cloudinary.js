74bc4c75792fb103796e0631d49bd0134e8249ee
Rishu <rishuranjan.edu@gmail.com>
2025-07-19 23:33:51 +0530

    db.js and cloudinary edited

--- Backend/lib/cloudinary.js
+++ Backend/lib/cloudinary.js
@@ -1,4 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// Load environment variables from the .env file in the project's root directory (one level up).
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -9,5 +15,7 @@ cloudinary.config({
 console.log(process.env.CLOUDINARY_CLOUD_NAME ,
   process.env.CLOUDINARY_API_KEY ,
   process.env.CLOUDINARY_API_SECRET );
+  console.log("MONGO_URI:", process.env.MONGO_URI ? "Loaded" : "MISSING!");
+  console.log("DB_NAME:", process.env.DB_NAME);
 
 export default cloudinary;
--- Backend/lib/db.js
+++ Backend/lib/db.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+    console.log("DB_NAME:", process.env.DB_NAME);
     const conn = await mongoose.connect(
       process.env.MONGO_URI + process.env.DB_NAME
     );
