71de2d187e1a774057bf54a9379430319c95996f
Rishu <rishuranjan.edu@gmail.com>
2025-07-21 11:36:18 +0530

    Increased image size limit

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "100mb" }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cart", cartRoutes);
