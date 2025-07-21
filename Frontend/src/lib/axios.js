ecd7b7142a57698f700c83cbc43223cbe45339ad
Rishu <rishuranjan.edu@gmail.com>
2025-07-21 12:20:13 +0530

    Axios changed

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
- baseURL: "https://cctvdigitalsurveillance.shop/api",
-  withCredentials: true, //INFO: Allows to send cookies with the request
+  baseURL: "https://cctvdigitalsurveillance.shop/api",
+  withCredentials: true, // Allows sending cookies with request
+  maxContentLength: Infinity,   // Allow huge JSON/form payloads
+  maxBodyLength: Infinity,      // Allow big file uploads
 });
 
 export default axiosInstance;
